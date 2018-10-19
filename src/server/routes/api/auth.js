15c7f15cdcc8c1229ec548eaab8732b279611896
Helene <Helene@Helenes-MacBook-Pro.local>
2018-10-19 16:55:20 +0200

    create contact, post and get routes client signaturit

--- src/server/routes/api/auth.js
+++ src/server/routes/api/auth.js
@@ -4,6 +4,9 @@ const User = require("../../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("../../config");
+const SignaturitClient = require("signaturit-sdk");
+
+let client = new SignaturitClient(config.SIGNATURIT_API_KEY, false); // false for sanbox, true for production
 
 router.post("/sign-up", (req, res) => {
     const { email, password } = req.body;
@@ -26,6 +29,48 @@ router.post("/sign-up", (req, res) => {
             const token = jwt.sign(cleanUser, config.SECRET_JWT_PASSPHRASE);
             res.send({ token });
         });
+});
+
+router.post("/getclient", (req, res) => {
+    const { id } = req.params;
+    console.log("AT post CLIENT");
+    recipient = { name: "Noe", email: "noelials@hotmail.es" };
+
+    files = ["../../public/Muster.pdf"];
+
+    sign_params = {
+        subject: "Receipt number 250",
+        body: "Please, can you sign this document?"
+    };
+
+    // client
+    //     .createSignature(files, [{ email: "noelials.otro@gmail.com" }])
+    //     .then(result => {
+    //         console.log("result create signature", result);
+    //     })
+    //     .catch(err => console.log("error create signature", err));
+
+    client
+        .createContact(recipient.email, recipient.name)
+        .then(result => {
+            console.log("result create contact", result);
+            client
+                .createSignature(files, recipient, sign_params)
+                .then(result => {
+                    console.log("result create signature", result);
+                })
+                .catch(err => console.log("error create signature", err));
+        })
+        .catch(err => console.log("error create contact", err));
+});
+
+router.get("/getclient", (req, res) => {
+    client
+        .getSignatures()
+        .then(result => {
+            console.log("result contacts", result);
+        })
+        .catch(err => console.log("error contacts", err));
 });
 
 router.post("/sign-in", (req, res) => {
--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const SignaturitClient = require("signaturit-sdk");
 
-client = new SignaturitClient("SIGNATURIT_API_KEY", false); // false for sanbox, true for production
+const axios = require("axios");
+
+const config = require("../../config");
 
 const authRoutes = require("./auth");
 const { userMiddleware, checkLoggedIn } = require("../../utils/middleware");
@@ -17,29 +18,6 @@ router.use("/auth", authRoutes);
 
 router.use((req, res) => {
     res.status(404).send({ error: "not-found" });
-});
-
-// new Signature request
-router.post("/send", (req, res) => {
-    // const { email } = req.body;
-
-    recipients = { email: req.body.email };
-
-    files = ["../../public/Muster.pdf"];
-
-    sign_params = {
-        subject: "Sign document",
-        body: "Please sign this document" // need to add the template here
-    };
-
-    client
-        .createSignature(files, recipients, sign_params)
-        .then(res => {
-            success: true;
-        })
-        .catch(error => {
-            success: false;
-        });
 });
 
 module.exports = router;
